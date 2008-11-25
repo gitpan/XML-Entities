6db007654e0c986a5f5496d0e408372fde7c17c7
Jan Oldřich Krůza <Sixtease+cpan@gmail.com>
2008-11-25 14:46:02 +0000

    Import of SIXTEASE/XML-Entities-0.0306 from CPAN.
    gitpan-cpan-distribution: XML-Entities
    gitpan-cpan-version:      0.0306
    gitpan-cpan-path:         SIXTEASE/XML-Entities-0.0306.tar.gz
    gitpan-cpan-author:       SIXTEASE
    gitpan-cpan-maturity:     released

--- lib/XML/Entities.pm
+++ lib/XML/Entities.pm
@@ -5,7 +5,7 @@ use 5.008;    # Unicode all over
 use Carp;
 use XML::Entities::Data;
 
-our $VERSION = '0.03_05';
+our $VERSION = '0.03_06';
 
 eval { require HTML::Parser };  # for fast XS implemented _decode_entities
 
